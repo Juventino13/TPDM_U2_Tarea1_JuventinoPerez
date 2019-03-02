9185982dd570bfaa839c1dffabdb88319909566a
Juventino13 <juventus139@hotmail.com>
2019-03-02 00:27:31 -0600

    feat:error

--- app/src/main/java/com/example/tpdm_u2_tarea1_juventinoperez/MainActivity.java
+++ app/src/main/java/com/example/tpdm_u2_tarea1_juventinoperez/MainActivity.java
@@ -2,12 +2,42 @@ package com.example.tpdm_u2_tarea1_juventinoperez;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    LinearLayout layin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        layin = findViewById(R.id.layoutdinamico);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menudinamico,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.insertar:
+
+                break;
+            case R.id.consultar:
+
+                break;
+            case R.id.modificar :
+
+                break;
+            case R.id.eliminar:
+
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

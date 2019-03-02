8ccc41420db872d61b9281194bbc00803a2da852
Juventino13 <juventus139@hotmail.com>
2019-03-01 23:32:36 -0600

    feat primer commit

---
+++ gradlew.bat
@@ -0,0 +1,24 @@
+[
+    {
+        "merged": "C:\\Users\\Juvetino_Asus\\AndroidStudioProjects\\TPDM_U1_Ejercicio2\\app\\build\\intermediates\\res\\merged\\debug\\color_abc_tint_seek_thumb.xml.flat",
+        "source": "C:\\Users\\Juvetino_Asus\\.gradle\\caches\\transforms-1\\files-1.1\\appcompat-v7-28.0.0.aar\\55ae45ee56736094d6a3b3ab7cefe905\\res\\color\\abc_tint_seek_thumb.xml"
+    },
+    {
+        "merged": "C:\\Users\\Juvetino_Asus\\AndroidStudioProjects\\TPDM_U1_Ejercicio2\\app\\build\\intermediates\\res\\merged\\debug\\drawable-v21_abc_list_divider_material.xml.flat",
+        "source": "C:\\Users\\Juvetino_Asus\\.gradle\\caches\\transforms-1\\files-1.1\\appcompat-v7-28.0.0.aar\\55ae45ee56736094d6a3b3ab7cefe905\\res\\drawable-v21\\abc_list_divider_material.xml"
+    },
+    {
+        "merged": "C:\\Users\\Juvetino_Asus\\AndroidStudioProjects\\TPDM_U1_Ejercicio2\\app\\build\\intermediates\\res\\merged\\debug\\anim_abc_slide_out_bottom.xml.flat",
+        "source": "C:\\Users\\Juvetino_Asus\\.gradle\\caches\\transforms-1\\files-1.1\\appcompat-v7-28.0.0.aar\\55ae45ee56736094d6a3b3ab7cefe905\\res\\anim\\abc_slide_out_bottom.xml"
+    },
+    {
+        "merged": "C:\\Users\\Juvetino_Asus\\AndroidStudioProjects\\TPDM_U1_Ejercicio2\\app\\build\\intermediates\\res\\merged\\debug\\drawable-mdpi-v4_abc_btn_switch_to_on_mtrl_00001.9.png.flat",
+        "source": "C:\\Users\\Juvetino_Asus\\.gradle\\caches\\transforms-1\\files-1.1\\appcompat-v7-28.0.0.aar\\55ae45ee56736094d6a3b3ab7cefe905\\res\\drawable-mdpi-v4\\abc_btn_switch_to_on_mtrl_00001.9.png"
+    },
+    {
+        "merged": "C:\\Users\\Juvetino_Asus\\AndroidStudioProjects\\TPDM_U1_Ejercicio2\\app\\build\\intermediates\\res\\merged\\debug\\drawable-xxhdpi-v4_abc_ic_menu_selectall_mtrl_alpha.png.flat",
+        "source": "C:\\Users\\Juvetino_Asus\\.gradle\\caches\\transforms-1\\files-1.1\\appcompat-v7-28.0.0.aar\\55ae45ee56736094d6a3b3ab7cefe905\\res\\drawable-xxhdpi-v4\\abc_ic_menu_selectall_mtrl_alpha.png"
+    },
+    {
+        "merged": "C:\\Users\\Juvetino_Asus\\AndroidStudioProjects\\TPDM_U1_Ejercicio2\\app\\build\\intermediates\\res\\merged\\debug\\drawable_abc_cab_background_internal_bg.xml.flat",
+        "source": "C:\\Users\\Juvetino_Asus\\.gradle\\caches\\transforms-1\\files-1.1\\appcompat-v

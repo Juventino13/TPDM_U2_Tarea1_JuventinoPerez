edb46589d55220c4d20c36549d45aa68300b68d9
Juventino13 <juventus139@hotmail.com>
2019-03-10 21:58:07 -0600

    feat: Select ListView

--- app/src/main/java/com/example/juvetino_asus/tpmd_u2_t1/BaseProyectoCivil.java
+++ app/src/main/java/com/example/juvetino_asus/tpmd_u2_t1/BaseProyectoCivil.java
@@ -1,10 +1,13 @@
 package com.example.juvetino_asus.tpmd_u2_t1;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class BaseProyectoCivil extends SQLiteOpenHelper {
+
+
 
     public BaseProyectoCivil( Context context, String name,  SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -23,4 +26,13 @@ public class BaseProyectoCivil extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
+
+public Cursor viewData(){
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        String query = "SELECT * FROM PROYECTOS";
+        Cursor cursor = db.rawQuery(query,null);
+return cursor;
+}
+
 }
--- app/src/main/java/com/example/juvetino_asus/tpmd_u2_t1/MainActivity.java
+++ app/src/main/java/com/example/juvetino_asus/tpmd_u2_t1/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.juvetino_asus.tpmd_u2_t1;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -14,14 +16,42 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    BaseProyectoCivil db;
+ArrayList<String> listItem;
+ArrayAdapter adapter;
+ListView userList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        db = new BaseProyectoCivil(this,"basesota",null,1);
+
+        listItem = new ArrayList<>();
+        userList = findViewById(R.id.lista);
+
+
+viewData();
+
+
+    userList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            String text = userList.getItemAtPosition(position).toString();
+            Toast.makeText(MainActivity.this,""+text, Toast.LENGTH_SHORT).show();
+
+        }
+    });
+
+
     }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -44,7 +74,20 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    private void viewData() {
+        Cursor cursor = db.viewData();
 
+        if (cursor.getCount()==0) {
+            Toast.makeText(this, "No hay datos ", Toast.LENGTH_LONG).show();
+        }else{
+            while (cursor.moveToNext()){
+                listItem.add(cursor.getString(1));
+
+            }
+            adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,listItem);
+            userList.setAdapter(adapter);
+        }
+    }
 
 
 
